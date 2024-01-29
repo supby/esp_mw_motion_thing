b4e9b5dcdb6750eb21ac1dc38debaca5de931019
Andre Suzanovich <supius@gmail.com>
2024-01-29 21:19:44 +0100

    add OTA

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,11 @@
 
 // Network
 #include <WiFi.h>
+#include <ESPmDNS.h>
 
-// #include <ESPAsyncWebServer.h>
-
-// AsyncWebServer server(80);
+// OTA
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 WebThingAdapter *adapter;
 const char *motionSensorTypes[] = {"MotionSensor", nullptr};
@@ -50,7 +51,6 @@ void setupWiFi(String deviceName) {
   String password;
 
   WiFi.mode(WIFI_STA);
-  //WiFi.mode(WIFI_AP_STA);
   WiFi.hostname(deviceName.c_str());
   WiFi.setAutoReconnect(true);
 
@@ -106,8 +106,6 @@ void setupWebThing(String deviceName) {
 }
 
 void checkProps() {
-  // MDNS.update();
-  
   uint out = digitalRead(LD2410_OUT_PIN);
 
   ThingPropertyValue newValue;
@@ -117,6 +115,50 @@ void checkProps() {
 
   adapter->update();
   
+}
+
+void setupOTA(const char* deviceName) {
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname(deviceName);
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
 }
 
 void setup() {
@@ -148,13 +190,15 @@ void setup() {
 
   setupWebThing(deviceName);
 
-  // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-  //   request->send(200, "text/plain", "Hello World");
-  // });   
-  // server.begin();
+  setupOTA(deviceName.c_str());
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
+  ArduinoOTA.handle();
   checkProps();
 }
 
